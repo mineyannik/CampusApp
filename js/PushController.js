b3ed39bec6837e8b2d2e43e9ecc2fd77e71b8a6d
Philipp <kirschem@dhbw-loerrach.de>
2017-09-04 11:54:14 +0200

    presentLocalNotification when app is active

--- js/PushController.js
+++ js/PushController.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import {AppState} from 'react-native'
 
 import FCM, {FCMEvent} from "react-native-fcm";
 import {feeds} from './util/Constants';
@@ -6,6 +7,9 @@ import {feeds} from './util/Constants';
 export default class PushController extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            key: ''
+        }
     }
 
     componentDidMount() {
@@ -20,9 +24,13 @@ export default class PushController extends Component {
 
         this.notificationListener = FCM.on(FCMEvent.Notification, notif => {
             console.log("Notification", notif);
-            this._handlePushNotification(notif);
+            if (notif.local_notification) {
+                this.props.handleNotification(this.state.key);
+            } else {
+                this._handlePushNotification(notif);
+            }
         });
-        
+
         this.refreshTokenListener = FCM.on(FCMEvent.RefreshToken, token => {
             console.log("TOKEN (refreshUnsubscribe)", token);
         });
@@ -36,7 +44,18 @@ export default class PushController extends Component {
     _handlePushNotification(notif) {
         if (notif !== null && notif.hasOwnProperty("newsfeed")) {
             const correspondingFeed = feeds.filter(feed => feed.key === notif.newsfeed);
-            this.props.handleNotification(correspondingFeed[0].key);
+            this.setState({key: correspondingFeed[0].key});
+            if (AppState.currentState === 'active' && !notif.opened_from_tray) {
+                FCM.presentLocalNotification({
+                    title: notif.fcm.title,
+                    body: notif.fcm.body,
+                    show_in_foreground: true,
+                    local: true
+                });
+            } else {
+                console.log('hellooo');
+                this.props.handleNotification(correspondingFeed[0].key);
+            }
         }
     }
 
